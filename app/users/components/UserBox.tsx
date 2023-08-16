870117584ddfacda61e6468af831916aa4d20998
Akash Shridharan <akashshridharan2802@gmail.com>
2023-08-16 23:08:13 +0530

    loading functionality create and add

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import {  User } from "@prisma/client";
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 // import LoadingModal from "@/app/components/modals/LoadingModal";
 
 interface UserBoxProps {
@@ -29,31 +30,36 @@ const UserBox: React.FC<UserBoxProps> = ({
     }, [data, router]);
 
   return (
-    <div onClick={handleClick} className="
-        w-full 
-          relative 
-          flex 
-          items-center 
-          space-x-3 
-          bg-white 
-          p-3 
-          hover:bg-neutral-100
-          rounded-lg
-          transition
-          cursor-pointer
-    ">
-        <Avatar user={data} />
-        <div className="min-w-0 flex-1">
-          <div className="focus:outline-none">
-            {/* <span className="absolute inset-0" aria-hidden="true" /> */}
-            <div className="flex justify-between items-center mb-1">
-              <p className="text-sm font-medium text-gray-900">
-                {data.name}
-              </p>
+    <>
+      {isLoading && (
+        <LoadingModal />
+      )}
+      <div onClick={handleClick} className="
+          w-full 
+            relative 
+            flex 
+            items-center 
+            space-x-3 
+            bg-white 
+            p-3 
+            hover:bg-neutral-100
+            rounded-lg
+            transition
+            cursor-pointer
+      ">
+          <Avatar user={data} />
+          <div className="min-w-0 flex-1">
+            <div className="focus:outline-none">
+              {/* <span className="absolute inset-0" aria-hidden="true" /> */}
+              <div className="flex justify-between items-center mb-1">
+                <p className="text-sm font-medium text-gray-900">
+                  {data.name}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-    </div>
+      </div>
+    </>
   )
 }
 
